5d9093840d76999f0c821b5101b9a6e3a2a7f181
Irina <46139426+iragerasimov@users.noreply.github.com>
2021-06-01 18:40:44 -0400

    Add files via upload

--- get_zot_pubs_and_notes.py
+++ get_zot_pubs_and_notes.py
@@ -2,6 +2,7 @@ import os
 import json
 import re
 import glob
+import time
 
 library_id = '2395775'
 library_type = 'group'
@@ -11,53 +12,52 @@ from pyzotero import zotero
 zot = zotero.Zotero(library_id, library_type, api_key)
 
 zot_doi_list = []
-refs_list = []
 zot_notes = []
 zot_pubs = []
+zot_atts = []
 json_folder_path = "data"
 zotero_path = "/home/igerasim/Zotero/storage/"
-cross_ref_file = "cross_refs_save1.json"
 regex = r"(?:[(<>)]|)"
 
-with open(os.path.join(json_folder_path, cross_ref_file)) as json_publications_file:
-  refs_list = json.load(json_publications_file)
+coll_key = {	'DIFs':'I28ER4KR',
+		'SCOPUS':'CIQKAU2P',
+		'WOS':'48XLCAWQ',
+		'ACOS_OCO':'649MDWWL',
+		'SCOPUS + WOS':'QJR2GCH8',
+		'Giovanni.2017':'TFS2QQD8',
+		'Aura_MLS':'F3P68PWK',
+		'Aura_OMI':'VVMITTJ5',
+		'GPM':'AHI8WD5Z',
+		'TRMM':'FBETQM7W',
+		'Giovanni':'KM3DFTCQ',
+		'Giovanni.reviewed':'KQJ5V2EC',
+		'MERRA-2':'RH6EGMYW',
+		'Forward_GESDISC':'8L3ZJKKV'
+	}
 
-def search_refs(refs_list, doi):
-  doi1 = re.sub(regex, '', doi)
-  for ref in refs_list:
-    doi2 = re.sub(regex, '', ref['new_doi'])
-    if re.search(doi1, doi2, re.IGNORECASE):
-      ref['zotero'] = 'yes'
-      return ref
-  return None
+coll = 'Forward_GESDISC'
 
 try:
-  #items = zot.collection_items('I28ER4KR', limit=10) # DIFs
-  #items = zot.collection_items('CIQKAU2P', limit=10) # SCOPUS
-  #items = zot.collection_items('48XLCAWQ', limit=10) # WOS
-  #items = zot.collection_items('649MDWWL', limit=10) # ACOS/OCO
-  #items = zot.collection_items('QJR2GCH8', limit=10) # SCOPUS + WOS
-  items = zot.collection_items('TFS2QQD8', limit=10) # Giovanni.2017
-  #items = zot.collection_items('F3P68PWK', limit=10) # Aura/MLS
+  items = zot.collection_items(coll_key[coll], limit=400) # Forward_GESDISC
+
   while True:
     for pub_item in items:
       if re.search("note", pub_item['data']['itemType']):
         zot_notes.append(pub_item)
       elif re.search("attachment", pub_item['data']['itemType']):
-        #print('This is attachment, skipping')
-        a=1
+        zot_atts.append(pub_item)
       else:
         zot_pubs.append(pub_item)
     items = zot.follow()
+    time.sleep(1)
 except StopIteration:
   print('\nAll items processed')
 except Exception as ex:
   print(ex)
 
-#with open(os.path.join(json_folder_path, "zot_notes_0504_2020.json"), "w") as fp:
-with open(os.path.join(json_folder_path, "zot_notes_gio.2017.json"), "w") as fp:
+with open(os.path.join(json_folder_path, "zot_notes_"+coll.lower()+".json"), "w") as fp:
   json.dump(zot_notes, fp, indent=4)
-#with open(os.path.join(json_folder_path, "zot_pubs_0504_2020.json"), "w") as fp:
-with open(os.path.join(json_folder_path, "zot_pubs_gio.2017.json"), "w") as fp:
+with open(os.path.join(json_folder_path, "zot_pubs_"+coll.lower()+".json"), "w") as fp:
   json.dump(zot_pubs, fp, indent=4)
-
+with open(os.path.join(json_folder_path, "zot_atts_"+coll.lower()+".json"), "w") as fp:
+  json.dump(zot_atts, fp, indent=4)
